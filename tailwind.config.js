4c2f2f4a02ed1ff6106a0d1a8028b7c47ddfaa2e
Testboiz <tvmini0202@gmail.com>
2024-09-24 04:57:39 +0700

    completed landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        custom_init: '#57544F',  // Replace with your color A
+        custom_mid: '#BAA16D',  // Replace with your color B
+        custom_end: '#BDB7AB',  // Replace with your color C
+      },
       fontFamily: {
         'sansita': ['"Sansita Swashed"', 'system-ui'],
       },
