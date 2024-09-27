ac9b21da5aff5436b832fbe33dff1e672ebc8a72
Testboiz <tvmini0202@gmail.com>
2024-09-27 15:16:19 +0700

    finished carousel for the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeOut: 'fadeOut 0.5s ease-in-out',
+      },
       colors: {
         custom_init: '#57544F',  // Replace with your color A
         custom_mid: '#BAA16D',  // Replace with your color B
         custom_end: '#BDB7AB',  // Replace with your color C
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
       },
       fontFamily: {
         'sansita': ['"Sansita Swashed"', 'system-ui'],
@@ -15,6 +29,11 @@ module.exports = {
         auto: 'auto',
       },
     },
+
   },
+  variants: {
+    animation: ['responsive', 'motion-safe', 'motion-reduce'],
+  },
+
 }
 
